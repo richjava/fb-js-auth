2a4fd7e07f9f9992fb1624e790c6256baf7b8c17
Richard Lovell <richjavalabs@gmail.com>
2016-01-13 11:33:25 +1300

    Removed redundant code

--- register.php
+++ register.php
@@ -43,47 +43,6 @@
         </div>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
-        <script>
-                // Load the SDK asynchronously
-                (function (thisdocument, scriptelement, id) {
-                    var js, fjs = thisdocument.getElementsByTagName(scriptelement)[0];
-                    if (thisdocument.getElementById(id))
-                        return;
-
-                    js = thisdocument.createElement(scriptelement);
-                    js.id = id;
-                    js.src = "//connect.facebook.net/en_US/sdk.js"; //you can use 
-                    fjs.parentNode.insertBefore(js, fjs);
-                }(document, 'script', 'facebook-jssdk'));
-
-                window.fbAsyncInit = function () {
-                    FB.init({
-                        appId: '849171741848303', //Your APP ID
-                        cookie: true, // enable cookies to allow the server to access 
-                        // the session
-                        xfbml: true, // parse social plugins on this page
-                        version: 'v2.1' // use version 2.1
-                    });
-
-                    // These three cases are handled in the callback function.
-//                    FB.getLoginStatus(function (response) {
-//                        statusChangeCallback(response);
-//                    });
-
-                };
-
-                // This is called with the results from from FB.getLoginStatus().
-                function statusChangeCallback(response) {
-                    if (response.status === 'connected') {
-                        // Logged into your app and Facebook.
-                        processFBRegistration();
-                    } else if (response.status === 'not_authorized') {
-                        // The person is logged into Facebook, but not your app.
-                        document.getElementById('status').innerHTML = 'Please log ' +
-                                'into this app.';
-                    }
-                }
-        </script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
         <script src="js/main.js"></script>
     </body>
